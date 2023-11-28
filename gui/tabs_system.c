ab8dd81b70add1cb686b65bdf70b3349f5890dbe
Param Patel <51967918+parampatel21@users.noreply.github.com>
2023-11-28 15:01:06 -0500

    test OS name

--- gui/tabs_system.c
+++ gui/tabs_system.c
@@ -1,27 +1,16 @@
 #include "tabs_system.h"
 #include <gtk/gtk.h>
-#include <sys/sysinfo.h>
-#include <stdio.h>
 
 void add_system_tab(GtkWidget *notebook) {
     GtkWidget *system_label = gtk_label_new("System");
-    GtkWidget *system_tab = gtk_label_new("");
 
-    // Gather system information
-    struct sysinfo si;
-    if (sysinfo(&si) != 0) {
-        perror("sysinfo");
-        return;
-    }
-
-    // Create a string to display system information
-    char info_str[512];
-    snprintf(info_str, sizeof(info_str),
-             "OS Release: %s\nKernel Version: %s\nMemory: %lu KB\nProcessor: %s\nDisk Storage: %lu KB",
-             g_unix_get_os_info(), g_unix_get_kernel_version(), si.totalram * si.mem_unit / 1024,
-             g_cpu_info_get_model(), si.totalswap * si.mem_unit / 1024);
-
-    gtk_label_set_text(GTK_LABEL(system_tab), info_str);
+//    get OS details
+    FILE *fp;
+    char OS_name[50] = " ";
+    fp = popen("lsb_release -ds", "r");
+    fgets(OS_name, 50, fp);
+    pclose(fp);
+    GtkWidget *system_tab = gtk_label_new(OS_name);
 
     gtk_notebook_append_page(GTK_NOTEBOOK(notebook), system_tab, system_label);
 }
