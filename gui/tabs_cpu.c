8f299f651c43364705705d362414a4ee5b62bad6
Param Patel <51967918+parampatel21@users.noreply.github.com>
2023-11-29 15:07:57 -0500

    test graphs v2

--- gui/tabs_cpu.c
+++ gui/tabs_cpu.c
@@ -1,130 +1,101 @@
+#include <gtk/gtk.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <gtk/gtk.h>
+#include <pthread.h>
 
-#define MAX_THREADS 100
+#define MAX_THREADS 10
+#define HISTORY_SIZE 60
+#define UPDATE_INTERVAL 1000
 
-typedef struct {
-    GtkWidget *drawing_area;
-    guint graph_update_timeout;
-    double thread_usage[MAX_THREADS];
-    int num_threads;
-} CPUData;
+GtkWidget *drawing_area;
+gdouble thread_usage[MAX_THREADS][HISTORY_SIZE] = {0};
+gint current_second = 0;
 
-static gboolean update_graph(gpointer user_data) {
-    CPUData *data = (CPUData *)user_data;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-    FILE *stat_file = fopen("/proc/stat", "r");
-    if (!stat_file) {
-        g_error("Failed to open /proc/stat file");
-        return FALSE;
+gboolean update_data(gpointer user_data) {
+    FILE *fp = fopen("/proc/stat", "r");
+    if (fp == NULL) {
+        perror("Error opening /proc/stat");
+        return TRUE;
     }
 
     char line[256];
-    if (!fgets(line, sizeof(line), stat_file)) {
-        g_error("Failed to read /proc/stat file");
-        fclose(stat_file);
-        return FALSE;
-    }
+    while (fgets(line, sizeof(line), fp)) {
+        if (strncmp(line, "cpu", 3) == 0) {
+            int cpu;
+            unsigned long user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
+            sscanf(line, "cpu %d %lu %lu %lu %lu %lu %lu %lu %lu %lu",
+                   &cpu, &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal, &guest, &guest_nice);
 
-    fclose(stat_file);
+            unsigned long total = user + nice + system + idle + iowait + irq + softirq + steal;
+            unsigned long idle_time = idle + iowait;
+            unsigned long total_diff, idle_diff;
 
-    unsigned long long total_time = 0;
-    char *token = strtok(line, " ");
-    while (token) {
-        token = strtok(NULL, " ");
-        if (token) {
-            total_time += strtoull(token, NULL, 10);
+            pthread_mutex_lock(&mutex);
+            total_diff = total - thread_usage[cpu][current_second % HISTORY_SIZE];
+            idle_diff = idle_time - thread_usage[cpu][current_second % HISTORY_SIZE];
+            thread_usage[cpu][current_second % HISTORY_SIZE] = total;
+            pthread_mutex_unlock(&mutex);
+
+            gtk_widget_queue_draw(drawing_area);
+
+            break;
         }
     }
 
-    FILE *status_file = fopen("/proc/self/status", "r");
-    if (!status_file) {
-        g_error("Failed to open /proc/self/status file");
-        return FALSE;
-    }
-
-    for (int i = 0; i < data->num_threads; ++i) {
-        char thread_line[256];
-        do {
-            if (!fgets(thread_line, sizeof(thread_line), status_file)) {
-                g_error("Failed to read /proc/self/status file");
-                fclose(status_file);
-                return FALSE;
-            }
-        } while (strncmp(thread_line, "Threads:", 8) != 0);
-
-        int num_threads;
-        if (sscanf(thread_line, "Threads: %d", &num_threads) != 1) {
-            g_error("Failed to parse Threads line");
-            fclose(status_file);
-            return FALSE;
-        }
-
-        data->num_threads = num_threads;
-
-        unsigned long long thread_time;
-        if (fscanf(status_file, " %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %llu", &thread_time) != 1) {
-            g_error("Failed to parse thread usage");
-            fclose(status_file);
-            return FALSE;
-        }
-
-        data->thread_usage[i] = (double)thread_time / total_time * 100.0;
-    }
-
-    fclose(status_file);
-
-    gtk_widget_queue_draw(data->drawing_area);
+    fclose(fp);
+    current_second = (current_second + 1) % HISTORY_SIZE;
 
     return TRUE;
 }
 
 static gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
-    CPUData *data = (CPUData *)user_data;
+    pthread_mutex_lock(&mutex);
 
-    int width, height;
-    gtk_widget_get_size_request(widget, &width, &height);
+    gint width = gtk_widget_get_allocated_width(widget);
+    gint height = gtk_widget_get_allocated_height(widget);
+    gdouble x_scale = (gdouble)width / HISTORY_SIZE;
+    gdouble y_scale = (gdouble)height / 100.0;
 
-    const GdkRGBA thread_colors[MAX_THREADS] = {
-        {0.0, 0.0, 1.0, 1.0}, // Blue
-        {1.0, 0.0, 0.0, 1.0}, // Red
-        {0.0, 1.0, 0.0, 1.0}, // Green
-    };
-
-    cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 1.0); // White
-    cairo_paint(cr);
-
+    cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);  // Blue color
     cairo_set_line_width(cr, 2.0);
 
-    for (int i = 0; i < data->num_threads; ++i) {
-        cairo_set_source_rgba(cr, thread_colors[i].red, thread_colors[i].green, thread_colors[i].blue, thread_colors[i].alpha);
+    for (int thread = 0; thread < MAX_THREADS; ++thread) {
+        for (int i = 0; i < HISTORY_SIZE - 1; ++i) {
+            gdouble x1 = i * x_scale;
+            gdouble y1 = height - thread_usage[thread][i] * y_scale;
+            gdouble x2 = (i + 1) * x_scale;
+            gdouble y2 = height - thread_usage[thread][i + 1] * y_scale;
 
-        double y = height * (1.0 - data->thread_usage[i] / 100.0);
-
-        cairo_move_to(cr, 0, y);
-        cairo_line_to(cr, width, y);
-
-        cairo_stroke(cr);
+            cairo_move_to(cr, x1, y1);
+            cairo_line_to(cr, x2, y2);
+        }
     }
+
+    cairo_stroke(cr);
+    pthread_mutex_unlock(&mutex);
 
     return FALSE;
 }
 
-void add_cpu_tab(GtkWidget *notebook) {
-    GtkWidget *cpu_label = gtk_label_new("CPU");
-    GtkWidget *cpu_tab = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
 
-    CPUData *data = g_malloc(sizeof(CPUData));
-    data->drawing_area = gtk_drawing_area_new();
-    gtk_widget_set_size_request(data->drawing_area, 400, 300);
-    gtk_box_pack_start(GTK_BOX(cpu_tab), data->drawing_area, TRUE, TRUE, 0);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "CPU Thread Monitor");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    g_signal_connect(G_OBJECT(data->drawing_area), "draw", G_CALLBACK(on_draw), data);
+    drawing_area = gtk_drawing_area_new();
+    gtk_container_add(GTK_CONTAINER(window), drawing_area);
+    g_signal_connect(drawing_area, "draw", G_CALLBACK(on_draw), NULL);
 
-    data->graph_update_timeout = g_timeout_add(1000, (GSourceFunc)update_graph, data);
+    gtk_widget_show_all(window);
 
-    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), cpu_tab, cpu_label);
+    g_timeout_add(UPDATE_INTERVAL, update_data, NULL);
+
+    gtk_main();
+
+    return 0;
 }
-
