1b4f7ec40674e2d5bfc7fd3f823c7aa0222d2b18
Param Patel <51967918+parampatel21@users.noreply.github.com>
2023-11-29 15:26:23 -0500

    add thread functionality

--- gui/tabs_cpu.c
+++ gui/tabs_cpu.c
@@ -1,17 +1,21 @@
 #include "tabs_cpu.h"
 #include <gtk/gtk.h>
+#include <dirent.h>
 
-const gchar* get_cpu_usage_string() {
-    FILE* fp = fopen("/proc/self/stat", "r");
+const gchar* get_thread_cpu_usage_string(pid_t tid) {
+    char path[256];
+    snprintf(path, sizeof(path), "/proc/self/task/%d/stat", tid);
+
+    FILE* fp = fopen(path, "r");
 
     if (fp == NULL) {
-        perror("Error opening /proc/self/stat");
+        perror("Error opening thread stat file");
         return "Error";
     }
 
     unsigned long utime, stime;
     if (fscanf(fp, "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*lu %*lu %*lu %lu %lu", &utime, &stime) != 2) {
-        perror("Error reading /proc/self/stat");
+        perror("Error reading thread stat file");
         fclose(fp);
         return "Error";
     }
@@ -22,15 +26,34 @@ const gchar* get_cpu_usage_string() {
     double seconds = 1.0;
     double cpu_usage = 100.0 * total_time / (sysconf(_SC_CLK_TCK) * seconds);
 
-    static gchar cpu_usage_str[50];
-    g_snprintf(cpu_usage_str, sizeof(cpu_usage_str), "CPU Usage: %.2f%%", cpu_usage);
+    static gchar thread_usage_str[50];
+    g_snprintf(thread_usage_str, sizeof(thread_usage_str), "Thread %d Usage: %.2f%%", tid, cpu_usage);
 
-    return cpu_usage_str;
+    return thread_usage_str;
+}
+
+void update_thread_cpu_usage(GtkLabel *label) {
+    DIR* task_dir = opendir("/proc/self/task");
+    if (task_dir == NULL) {
+        perror("Error opening task directory");
+        return;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(task_dir)) != NULL) {
+        if (entry->d_type == DT_DIR && isdigit(entry->d_name[0])) {
+            pid_t tid = atoi(entry->d_name);
+            const gchar *thread_usage_str = get_thread_cpu_usage_string(tid);
+            gtk_label_set_text(label, thread_usage_str);
+            g_usleep(100000);
+        }
+    }
+
+    closedir(task_dir);
 }
 
 gboolean update_timer_callback(GtkLabel *label) {
-    const gchar *cpu_usage_str = get_cpu_usage_string();
-    gtk_label_set_text(label, cpu_usage_str);
+    update_thread_cpu_usage(label);
     return TRUE;
 }
 
@@ -43,5 +66,4 @@ void add_cpu_tab(GtkWidget *notebook) {
     gtk_notebook_append_page(GTK_NOTEBOOK(notebook), cpu_tab, cpu_label);
 
     guint timer_id = g_timeout_add_seconds(1, (GSourceFunc)update_timer_callback, cpu_label_widget);
-
 }
