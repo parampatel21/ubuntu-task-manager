9643a25ff3a68f5dfa85ea6f57801c3228ef10a8
Param Patel <51967918+parampatel21@users.noreply.github.com>
2023-11-29 16:03:16 -0500

    mpstat core usage

--- gui/tabs_cpu.c
+++ gui/tabs_cpu.c
@@ -1,58 +1,62 @@
-#include <stdio.h>
-#include <string.h>
+#include "tabs_cpu.h"
 #include <gtk/gtk.h>
 
-GtkWidget *cpu_tab;
+// Structure to hold data for each core
+typedef struct {
+    GtkWidget *label;
+    int core_number;
+} CoreData;
 
-void update_cpu_tab(GtkWidget *labels[]) {
+// Function to update core usage labels
+void update_core_labels(CoreData *core_data) {
     FILE *mpstat_fp;
     char mpstat_buffer[5000];
-    mpstat_fp = popen("mpstat -P ALL 1 1", "r");  // Run the command for 1 second to get updated values
+    mpstat_fp = popen("mpstat -P ALL 1 1", "r"); // Run the command for 1 second to get updated values
     fgets(mpstat_buffer, sizeof(mpstat_buffer), mpstat_fp);
     pclose(mpstat_fp);
 
-    char *line = strtok(mpstat_buffer, "\n");
-    int core_index = 0;
-
-    while (line != NULL) {
-        if (strstr(line, "all") == NULL && strstr(line, "CPU") == NULL) {
-            double usage;
-            sscanf(line + 13, "%lf", &usage);
-
-            char label_text[50];
-            snprintf(label_text, sizeof(label_text), "Core %d: %.2f%%", core_index, usage);
-
-            gtk_label_set_text(GTK_LABEL(labels[core_index]), label_text);
-            core_index++;
-        }
-
+    // Find the line with core usage information
+    char *line = strstr(mpstat_buffer, "all");
+    if (line != NULL) {
+        // Move to the next line to get the core details
         line = strtok(NULL, "\n");
+
+        while (line != NULL) {
+            int core;
+            float usage;
+            sscanf(line, "%*s %d %*f %*f %*f %*f %*f %*f %*f %*f %*f %f", &core, &usage);
+
+            // Update the corresponding label
+            if (core_data[core].label != NULL) {
+                char text[50];
+                snprintf(text, sizeof(text), "Core %d: %.2f%%", core, usage);
+                gtk_label_set_text(GTK_LABEL(core_data[core].label), text);
+            }
+
+            line = strtok(NULL, "\n");
+        }
     }
 }
 
-gboolean update_cpu_tab_timeout(gpointer user_data) {
-    GtkWidget **labels = (GtkWidget **)user_data;
-    update_cpu_tab(labels);
-    return G_SOURCE_CONTINUE;
-}
-
+// Function to create the CPU tab
 void add_cpu_tab(GtkWidget *notebook) {
     GtkWidget *cpu_label = gtk_label_new("CPU");
+    GtkWidget *cpu_tab = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
 
-    // Create a grid to organize labels
-    GtkWidget *grid = gtk_grid_new();
-    gtk_container_add(GTK_CONTAINER(cpu_tab), grid);
+    // Array to hold core data
+    int num_cores = 8; // Change this according to the number of cores on your system
+    CoreData core_data[num_cores];
 
-    // Create an array of labels for each core
-    GtkWidget *core_labels[8];
-    for (int i = 0; i < 8; i++) {
-        core_labels[i] = gtk_label_new("");
-        gtk_grid_attach(GTK_GRID(grid), core_labels[i], 0, i, 1, 1);
+    // Create labels for each core
+    for (int i = 0; i < num_cores; i++) {
+        core_data[i].label = gtk_label_new(NULL);
+        core_data[i].core_number = i;
+        gtk_box_pack_start(GTK_BOX(cpu_tab), core_data[i].label, FALSE, FALSE, 0);
     }
 
-    // Append the grid to the notebook
+    // Add the CPU tab to the notebook
     gtk_notebook_append_page(GTK_NOTEBOOK(notebook), cpu_tab, cpu_label);
 
-    // Update the CPU tab every half second
-    g_timeout_add(500, update_cpu_tab_timeout, core_labels);
+    // Set up a timer to update core labels every half second
+    g_timeout_add(500, (GSourceFunc)update_core_labels, core_data);
 }
