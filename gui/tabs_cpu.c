889d5e9a914c846ddc172aa4d3c78d27516f7b1a
Param Patel <51967918+parampatel21@users.noreply.github.com>
2023-11-29 22:57:49 -0500

    revert to previous

--- gui/tabs_cpu.c
+++ gui/tabs_cpu.c
@@ -1,77 +1,28 @@
 #include "tabs_cpu.h"
 #include <gtk/gtk.h>
 
-int core_count = 0;
-
-double *cpu_usage = NULL;
-
-void update_cpu_usage() {
-    FILE *mpstat_pipe = popen("mpstat -P ALL 1 1 | tail -n +4 | awk '{print 100 - $NF}'", "r");
-    if (mpstat_pipe == NULL) {
-        g_print("Error opening pipe!\n");
-        return;
-    }
-
-    fscanf(mpstat_pipe, "%lf", &cpu_usage[0]);
-
-    for (int i = 1; i <= core_count; i++) {
-        fscanf(mpstat_pipe, "%lf", &cpu_usage[i]);
-    }
-
-    pclose(mpstat_pipe);
-}
-
-gboolean draw_cpu_graph(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
-    int width, height;
-    gtk_widget_get_size_request(widget, &width, &height);
-
-    cairo_set_source_rgb(cr, 1, 1, 1);  // White color
-    cairo_paint(cr);
-
-    cairo_set_source_rgb(cr, 0, 0, 0);  // Black color
-
-    cairo_set_line_width(cr, 2.0);
-    for (int i = 0; i <= core_count; i++) {
-        double x = (double)i / core_count * width;
-        double y = height - cpu_usage[i] * height / 100.0;
-        if (i == 0) {
-            cairo_move_to(cr, x, y);
-        } else {
-            cairo_line_to(cr, x, y);
-        }
-    }
-
-    cairo_stroke(cr);
-
-    return FALSE;
-}
-
-gboolean update_cpu_data(gpointer user_data) {
-    update_cpu_usage();
-
-    GtkWidget *drawing_area = GTK_WIDGET(user_data);
-    gtk_widget_queue_draw(drawing_area);
-
-    return TRUE;
-}
 
 void add_cpu_tab(GtkWidget *notebook) {
     GtkWidget *cpu_label = gtk_label_new("CPU");
-    GtkWidget *grid = gtk_grid_new();
-    gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
 
-    core_count = atoi(popen("nproc --all", "r"));
+    int core_count = atoi(popen("nproc --all", "r"));
 
-    cpu_usage = g_malloc((core_count + 1) * sizeof(double));
+//    I want to get the output of this command every second: mpstat -P ALL | tail -n +4 | awk '{print 100 - $NF}'
+//    Example output (first line is always total of all cores, there are core_count lines afterwards):
+//    1.96
+//    2.94
+//    2.29
+//    2.56
+//    1.98
+//    2.89
+//    2.41
+//    2.47
+//    2.11
 
-    GtkWidget *drawing_area = gtk_drawing_area_new();
-    gtk_widget_set_size_request(drawing_area, 400, 200);
-    g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(draw_cpu_graph), NULL);
-    gtk_grid_attach(GTK_GRID(grid), drawing_area, 0, 0, 1, 1);
+//  I should plot the line afterward on a GTK graph, adding new data in every single
+//  time I get the output from the command above
 
-    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), grid, cpu_label);
-
-    update_cpu_usage();
-
-    g_timeout_add_seconds(1, update_cpu_data, drawing_area);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), box, cpu_label);
 }
+
