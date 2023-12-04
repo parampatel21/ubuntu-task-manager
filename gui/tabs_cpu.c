d955452cc9c8d78909eb8a8f744ca484d3a7c892
Param Patel <51967918+parampatel21@users.noreply.github.com>
2023-12-03 19:39:22 -0500

    graph.c works integration does not

--- gui/tabs_cpu.c
+++ gui/tabs_cpu.c
@@ -3,12 +3,9 @@
 
 
 void add_cpu_tab(GtkWidget *notebook) {
-/*    GtkWidget *cpu_label = gtk_label_new("CPU");
+    GtkWidget *cpu_label = gtk_label_new("CPU");
     GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
 
-    int core_count = atoi(popen("nproc --all", "r"));
-
     gtk_notebook_append_page(GTK_NOTEBOOK(notebook), box, cpu_label);
-*/
 }
 
