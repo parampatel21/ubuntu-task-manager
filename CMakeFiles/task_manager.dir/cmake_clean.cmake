b8dd7c12ba9fe6ff21751a28fc4cb3174bebbf29
Param Tejal Patel <ptpatel@data.cs.purdue.edu>
2023-12-04 23:48:14 -0500

    final commit

---
+++ CMakeFiles/task_manager.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_manager.dir/gui/tabs_cpu.c.o"
+  "CMakeFiles/task_manager.dir/gui/tabs_cpu.c.o.d"
+  "CMakeFiles/task_manager.dir/gui/tabs_memory.c.o"
+  "CMakeFiles/task_manager.dir/gui/tabs_memory.c.o.d"
+  "CMakeFiles/task_manager.dir/gui/tabs_process.c.o"
+  "CMakeFiles/task_manager.dir/gui/tabs_process.c.o.d"
+  "CMakeFiles/task_manager.dir/gui/tabs_system.c.o"
+  "CMakeFiles/task_manager.dir/gui/tabs_system.c.o.d"
+  "CMakeFiles/task_manager.dir/main.c.o"
+  "CMakeFiles/task_manager.dir/main.c.o.d"
+  "task_manager"
+  "task_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/task_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
